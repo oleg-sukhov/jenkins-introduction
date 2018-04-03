63c33d7758f74ed0ed4bd6557e2fc913e3402091
Oleg Sukhov <osukhov21@gmail.com>
2018-04-03 09:34:07 +0300

    JNK-0001: Initial commit

---
+++ src/main/java/ua/vn/os/jenkinsintro/JenkinsIntroApplication.java
@@ -0,0 +1,12 @@
+package ua.vn.os.jenkinsintro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsIntroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsIntroApplication.class, args);
+	}
+}
---
+++ src/test/java/ua/vn/os/jenkinsintro/JenkinsIntroApplicationTests.java
@@ -0,0 +1,16 @@
+package ua.vn.os.jenkinsintro;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JenkinsIntroApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
